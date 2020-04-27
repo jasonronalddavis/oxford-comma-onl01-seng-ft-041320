261902f33430b30e3643c8e2514a7c17bedccf9c
Jason Davis <jdavis@c.ringling.edu>
2020-04-27 17:22:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array = "kiwi, durian"
+  puts array.join(" and ")
 end
+end
+#.push "starfruit".unshift "kiwi"
+
+#def oxford_comma(array)
+#array.join(" and ").push "starfruit".unshift "kiwi"
+#end
