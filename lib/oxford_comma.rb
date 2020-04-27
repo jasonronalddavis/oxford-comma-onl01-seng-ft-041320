b6150ff43076d4aa1b17d72c76894b22d1413ed6
Jason Davis <jdavis@c.ringling.edu>
2020-04-27 18:07:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
 if array.size == 2
  array.join(" and ")
  elsif array.size == 3
-  .array.join(" and ") array.push "starfruit" array.unshift "kiwi"
+  .array.join(" and ") 
+  array.push "starfruit" array.unshift "kiwi"
 end
 end
 
